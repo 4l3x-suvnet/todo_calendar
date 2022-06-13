d8317e4ce69f5926a8088bde0a8440b3b12bde2c
4l3x <alexander.auermaa@gmail.com>
2022-06-13 10:14:15 +0200

    Todos can now be removed

--- js/todos.js
+++ js/todos.js
@@ -1,16 +1,28 @@
 let todoItems = [];
 
-const add = document.querySelector(".add");
+const add = document.querySelector('.add');
 
 function renderTodo(todo) {
-  const list = document.querySelector(".todo-list");
-  const item = document.createElement("div");
+  const list = document.querySelector('.todo-list');
+  const itemContainer = document.createElement('div');
+  const item = document.createElement('div');
+  const remove = document.createElement('button');
 
-  item.classList.add("item");
+  itemContainer.classList.add('item-container');
+  item.classList.add('item');
 
   item.innerHTML = todo.text;
+  remove.innerHTML = 'Remove';
 
-  list.append(item);
+  list.append(itemContainer);
+  itemContainer.append(item);
+  itemContainer.append(remove);
+
+  remove.addEventListener('click', () => removeTodo(itemContainer));
+}
+
+function removeTodo(itemContainer) {
+  itemContainer.parentNode.removeChild(itemContainer);
 }
 
 function addTodo(text) {
@@ -22,13 +34,13 @@ function addTodo(text) {
   renderTodo(todo);
 }
 
-add.addEventListener("click", () => {
-  const input = document.querySelector(".todo-input");
+add.addEventListener('click', () => {
+  const input = document.querySelector('.todo-input');
 
   const text = input.value;
 
-  if (text !== "") {
+  if (text !== '') {
     addTodo(text);
-    input.value = "";
+    input.value = '';
   }
 });
