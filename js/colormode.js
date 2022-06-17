d6734c30b8eb714e953ca95dc83c5666b2bcdd1e
4l3x <alexander.auermaa@gmail.com>
2022-06-18 01:41:17 +0200

    data-theme introduced

---
+++ js/colormode.js
@@ -0,0 +1,19 @@
+// Sets default theme
+// TODO: Save theme to localstorage
+setDataTheme("dark-mode");
+
+document.querySelector("#colorMode").addEventListener('click', () => {
+    
+    var element = document.body;
+    element.classList.toggle("light-mode");
+    if(document.body.classList.contains("light-mode")) {
+        setDataTheme("light-mode");
+    }
+    else {
+       setDataTheme("dark-mode");
+   }
+});
+
+function setDataTheme(theme) {
+    document.documentElement.setAttribute("data-theme", theme);
+  }
