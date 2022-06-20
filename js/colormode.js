5655b07bfbb9010016f6315340f1ef1b8bac0c10
Annangggg <90825899+Annangggg@users.noreply.github.com>
2022-06-21 00:05:02 +0200

    button state

--- js/colormode.js
+++ js/colormode.js
@@ -1,20 +1,30 @@
 // TODO: Save button state to local storage
 let theme = JSON.parse(localStorage.getItem("data-theme"));
 
-if (!theme) theme = 'dark-mode';
+changeModeByChecked();
 
-document.querySelector("#colorMode").addEventListener('click', () => {
+if (!theme) theme = 'dark-mode';
+document.getElementById("colorMode").addEventListener('click', () => {
     
     var element = document.body;
     element.classList.toggle("light-mode");
 
-    if(document.body.classList.contains("light-mode")) setDataTheme("light-mode");
-    else setDataTheme("dark-mode");
+    if(document.body.classList.contains("light-mode")) setDataTheme("dark-mode");
+    else setDataTheme("light-mode");
 
 });
 
 function setDataTheme(theme) {
     document.documentElement.setAttribute("data-theme", theme);
     localStorage.setItem("data-theme", JSON.stringify(theme));
+  }
+
+  function changeModeByChecked(){
+    if(theme === 'light-mode') {
+        document.getElementById("colorMode").checked = false;
+    }
+    else {
+        document.getElementById("colorMode").checked = true;
+    } 
   }
 
