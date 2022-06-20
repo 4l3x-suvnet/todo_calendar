8ddc6d1c850d839f8b208974dbe88c0ab1044ba4
4l3x <alexander.auermaa@gmail.com>
2022-06-20 16:36:48 +0200

    Theme now saves to local storage

--- js/colormode.js
+++ js/colormode.js
@@ -1,17 +1,20 @@
-// TODO: Save theme to localstorage
+// TODO: Save button state to local storage
+let theme = JSON.parse(localStorage.getItem("data-theme"));
+
+if (!theme) theme = 'dark-mode';
 
 document.querySelector("#colorMode").addEventListener('click', () => {
     
     var element = document.body;
     element.classList.toggle("light-mode");
-    if(document.body.classList.contains("light-mode")) {
-        setDataTheme("light-mode");
-    }
-    else {
-       setDataTheme("dark-mode");
-   }
+
+    if(document.body.classList.contains("light-mode")) setDataTheme("light-mode");
+    else setDataTheme("dark-mode");
+
 });
 
 function setDataTheme(theme) {
     document.documentElement.setAttribute("data-theme", theme);
+    localStorage.setItem("data-theme", JSON.stringify(theme));
   }
+
