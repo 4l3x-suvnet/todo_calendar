78f81cdb3143faf4c0c3d305f89e331ce61abea3
4l3x <alexander.auermaa@gmail.com>
2022-06-22 17:32:45 +0200

    Hovering over weather divs now shows additional weather information

--- js/weather.js
+++ js/weather.js
@@ -19,20 +19,82 @@ function getWeatherForToday(city) {
   })
   .then((myJson) => {
     
-    icon = mapWeatherIcon[myJson.weather[0].icon];
-    desc = myJson.weather[0].description;
-    wind = myJson.wind.speed;
-    humidity = myJson.main.humidity;
-    temperature = myJson.main.temp;
+    const icon = mapWeatherIcon[myJson.weather[0].icon];
 
-    weather = `<i class="fa-solid ${icon}"></i>`
-    weather += `<span id="weatherInfo">
-    <div>${desc}</div>
-    <div class="wind"><i class="fa-solid fa-wind"></i><div>${wind}m/s</div></div>
-    <div class="humidity"><i class="fa-solid fa-droplet"></i><div>${humidity}%</div></div>
-    </span>`
-    weather += `Temp: ${temperature.toFixed(1)}°C`;
-    document.querySelector(".weather").innerHTML = weather;
+    const desc = document.createElement('div');
+    desc.innerHTML = myJson.weather[0].description;
+    // Should be in descContainer
+    const wind = document.createElement('div');
+    wind.innerHTML = myJson.wind.speed + "m/s";
+    const humidity = document.createElement('div');
+    humidity.innerHTML = myJson.main.humidity + "%";
+
+    const temperature = myJson.main.temp;
+    // Should be in tempContainer
+    const minTemp = document.createElement('div');
+    minTemp.innerHTML = myJson.main.temp_min + "°C";
+    const maxTemp = document.createElement('div');
+    maxTemp.innerHTML = myJson.main.temp_max + "°C";
+
+    const container = document.querySelector(".weather");
+    const iconContainer = document.createElement('i');
+    const descContainer = document.createElement('div');
+    const windContainer = document.createElement('div');
+    const windIcon = document.createElement('i');
+    const humidityContainer = document.createElement('div');
+    const humidityIcon = document.createElement('i');
+    
+    const tempContainer = document.createElement('div');
+    const degree = document.createElement("i");
+    const celsius = document.createElement("i");
+    const otherTempsContainer = document.createElement('div');
+    const minTempIcon = document.createElement('i');
+    const minTempContainer = document.createElement('div');
+    const maxTempContainer = document.createElement('div');
+    const maxTempIcon = document.createElement('i');
+
+    iconContainer.classList.add("weatherIcon", "fa-solid", `${icon}`);
+    
+    descContainer.id = "weatherInfo";
+    otherTempsContainer.id = "tempInfo";
+    
+    windContainer.classList.add("d-flex", "justify-space-between");
+    windIcon.classList.add("fa-solid", "fa-wind");
+    
+    humidityContainer.classList.add("d-flex", "justify-space-between");
+    humidityIcon.classList.add("fa-solid", "fa-droplet");
+    
+    tempContainer.classList.add("tempIcons");
+    degree.classList.add("fa-solid", "fa-circle-dot");
+    celsius.classList.add("fa-solid", "fa-c");
+    
+    minTempContainer.classList.add("d-flex", "justify-space-between");
+    minTempIcon.classList.add("fa-solid", "fa-temperature-low");
+
+    maxTempContainer.classList.add("d-flex", "justify-space-between");
+    maxTempIcon.classList.add("fa-solid", "fa-temperature-high");
+
+    // Converts temp string to font-awesome icons
+    for (let index = 0; index < temperature.toFixed(1).length; index++) {
+      const newIcon = document.createElement('i');
+      newIcon.classList.add("fa-solid")
+      newIcon.classList.add(`${mapTemperature[temperature.toFixed(1)[index]]}`)
+      tempContainer.append(newIcon);
+    }
+
+    tempContainer.append(degree, celsius);
+    minTempContainer.append(minTempIcon, minTemp);
+    maxTempContainer.append(maxTempIcon, maxTemp);
+    otherTempsContainer.append(minTempContainer, maxTempContainer)
+    
+    descContainer.append(desc);
+    windContainer.append(windIcon, wind);
+    descContainer.append(windContainer);
+    
+    humidityContainer.append(humidityIcon, humidity);
+    descContainer.append(humidityContainer);
+    
+    container.append(iconContainer, descContainer, tempContainer, otherTempsContainer);
   });
 }
 
@@ -56,6 +118,21 @@ const mapWeatherIcon = {
   "50d": "fa-smog",
   "50n": "fa-smog"
 };
+
+const mapTemperature = {
+  "-": "fa-minus",
+  "0": "fa-0",
+  "1": "fa-1",
+  "2": "fa-2",
+  "3": "fa-3",
+  "4": "fa-4",
+  "5": "fa-5",
+  "6": "fa-6",
+  "7": "fa-7",
+  "8": "fa-8",
+  "9": "fa-9",
+  ".": "fa-circle",
+}
 
 function errorHandling(error) {
   getWeatherForToday(defaultCity);
